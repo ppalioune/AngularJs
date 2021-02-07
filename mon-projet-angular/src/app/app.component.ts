b04fe3120590daa47aa0aab1cd94892f702f270e
ppalioune <ppalioune@gmail.com>
2021-02-07 18:37:45 -0500

    premier exemple: interpolation, binding, 2WB, propriétes perso

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my awesome app';
+  //title = 'my awesome app';
+  isAuth = false
+
+  appareilOne = "Machine a laver"
+  appareilTwo = "Frigo"
+  appareilThree = "Ordinateur"
+  constructor(){
+    setTimeout(()=>{
+      this.isAuth = true
+    }, 4000
+    )
+  }
+  onAllumer(){
+    console.log("allumer");
+    
+  }
 }
